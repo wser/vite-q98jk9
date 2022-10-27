d8643fd10c78c28733d75dc0659b2e13d50635c3
wser <w.web.dev@gmail.com>
2022-10-27 13:35:33 +0200

    3 files modified

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { SimpleSearch } from 'vitepress-plugin-simple-search';
+import { VitePWA } from 'vite-plugin-pwa';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [SimpleSearch()],
+  plugins: [SimpleSearch(), VitePWA({ registerType: 'autoUpdate' })],
 });
