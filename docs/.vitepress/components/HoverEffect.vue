ad410a56c649b8e050386cd925215b35f20eccf3
wser <w.web.dev@gmail.com>
2022-11-15 08:58:35 +0100

    1 file modified, 2 added

--- docs/.vitepress/components/HoverEffect.vue
+++ docs/.vitepress/components/HoverEffect.vue
@@ -1,79 +1,103 @@
 <template>
-  <h2>Mouse Hover effect</h2>
+  <div class="main">
+    <h2>Mouse Hover effect</h2>
 
-  <div class="grid-container">
-    <div class="mask top"></div>
-    <div class="mask left"></div>
-    <div class="content">
-      <div class="cal">
-        <div class="day" tabindex="0">1</div>
-        <div class="day" tabindex="0">2</div>
-        <div class="day" tabindex="0">3</div>
-        <div class="day" tabindex="0">4</div>
-        <div class="day" tabindex="0">5</div>
-        <div class="day" tabindex="0">6</div>
-        <div class="day" tabindex="0">7</div>
-        <div class="day" tabindex="0">8</div>
-        <div class="day" tabindex="0">9</div>
-        <div class="day" tabindex="0">10</div>
-        <div class="day" tabindex="0">11</div>
-        <div class="day" tabindex="0">12</div>
-        <div class="day" tabindex="0">13</div>
-        <div class="day" tabindex="0">14</div>
-        <div class="day" tabindex="0">15</div>
-        <div class="day" tabindex="0">16</div>
-        <div class="day" tabindex="0">17</div>
-        <div class="day" tabindex="0">18</div>
-        <div class="day" tabindex="0">19</div>
-        <div class="day" tabindex="0">20</div>
-        <div class="day" tabindex="0">21</div>
-        <div class="day" tabindex="0">22</div>
-        <div class="day" tabindex="0">23</div>
-        <div class="day" tabindex="0">24</div>
-        <div class="day" tabindex="0">25</div>
-        <div class="day" tabindex="0">26</div>
-        <div class="day" tabindex="0">27</div>
-        <div class="day" tabindex="0">28</div>
-        <div class="day" tabindex="0">29</div>
-        <div class="day" tabindex="0">30</div>
-        <div class="day" tabindex="0">31</div>
-        <div class="day">
-          <div class="oom">1</div>
-        </div>
-        <div class="day">
-          <div class="oom">2</div>
-        </div>
-        <div class="day">
-          <div class="oom">3</div>
-        </div>
-        <div class="day">
-          <div class="oom">4</div>
+    <div class="themes">
+      <ul>
+        <li class="theme-default" ref="themes">1</li>
+        <li class="theme-2" ref="themes1">2</li>
+        <li class="theme-3" ref="themes">3</li>
+        <li class="theme-4" ref="themes">4</li>
+        <li class="theme-5" ref="themes">5</li>
+        <li class="theme-6" ref="themes">6</li>
+      </ul>
+    </div>
+    <div class="grid-container">
+      <div class="mask top"></div>
+      <div class="mask left"></div>
+      <div class="content">
+        <div class="cal">
+          <div class="day" tabindex="0">1</div>
+          <div class="day" tabindex="0">2</div>
+          <div class="day" tabindex="0">3</div>
+          <div class="day" tabindex="0">4</div>
+          <div class="day" tabindex="0">5</div>
+          <div class="day" tabindex="0">6</div>
+          <div class="day" tabindex="0">7</div>
+          <div class="day" tabindex="0">8</div>
+          <div class="day" tabindex="0">9</div>
+          <div class="day" tabindex="0">10</div>
+          <div class="day" tabindex="0">11</div>
+          <div class="day" tabindex="0">12</div>
+          <div class="day" tabindex="0">13</div>
+          <div class="day" tabindex="0">14</div>
+          <div class="day" tabindex="0">15</div>
+          <div class="day" tabindex="0">16</div>
+          <div class="day" tabindex="0">17</div>
+          <div class="day" tabindex="0">18</div>
+          <div class="day" tabindex="0">19</div>
+          <div class="day" tabindex="0">20</div>
+          <div class="day" tabindex="0">21</div>
+          <div class="day" tabindex="0">22</div>
+          <div class="day" tabindex="0">23</div>
+          <div class="day" tabindex="0">24</div>
+          <div class="day" tabindex="0">25</div>
+          <div class="day" tabindex="0">26</div>
+          <div class="day" tabindex="0">27</div>
+          <div class="day" tabindex="0">28</div>
+          <div class="day" tabindex="0">29</div>
+          <div class="day" tabindex="0">30</div>
+          <div class="day" tabindex="0">31</div>
+          <div class="day">
+            <div class="oom">1</div>
+          </div>
+          <div class="day">
+            <div class="oom">2</div>
+          </div>
+          <div class="day">
+            <div class="oom">3</div>
+          </div>
+          <div class="day">
+            <div class="oom">4</div>
+          </div>
         </div>
       </div>
+      <div class="mask right"></div>
+      <div class="mask bottom"></div>
     </div>
-    <div class="mask right"></div>
-    <div class="mask bottom"></div>
+
+    <div id="light" ref="light" :style="`left: ${x}px; top: ${y}px;`"></div>
+    <p>x: {{ x }} y: {{ y }}</p>
   </div>
-  <div id="light" :style="`left: ${x}px; top: ${y}px;`"></div>
-  <p>x: {{ x }} y: {{ y }}</p>
 </template>
 
 <script setup>
-import { useMouse } from '@vueuse/core';
+import { useMouse } from './functions/mouse.js';
 
-const { x, y } = useMouse({ touch: false });
+const { x, y } = useMouse();
+
+// import { useMouse } from '@vueuse/core';
+
+// const { x, y } = useMouse({ touch: false });
+
+// const light = document.getElementById('light')
+// document.addEventListener('mousemove', e => {
+//   light.style.top = e.pageY + "px"
+//   light.style.left = e.pageX + "px"
+// })
+
+// for (i = 0; i < themes.length; ++i) {
+//   themes[i].addEventListener('click', (e) => {
+//     document.documentElement.classList = [e.target.classList.value];
+//   });
+// }
 </script>
 
 <style scoped>
-/* body,
-html {
+.main {
   height: 100%;
   margin: 0;
-}
-*/
-body {
   overflow: hidden;
-  font-family: courier;
   font-weight: bold;
 }
 
@@ -108,8 +132,9 @@ body {
   height: 15rem;
   z-index: 4;
   border-radius: 50%;
-  transform: translate3d(-50%, -50%, 0);
-  /* background-color: white; */
+  /* transform: translate3d(-50%, -50%, 0); */
+  left: 10px;
+  top: 10px;
 }
 
 .grid-container {
@@ -144,6 +169,32 @@ body {
 .mask {
   position: relative;
   z-index: 99;
+}
+
+.themes {
+  position: relative;
+  z-index: 100;
+}
+.themes ul {
+  margin: 0;
+  display: flex;
+  padding: 2rem;
+  justify-content: center;
+  align-items: center;
+}
+.themes li {
+  width: 3rem;
+  height: 3rem;
+  position: relative;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 1.4rem;
+  border: 2px solid transparent;
+}
+.themes li:hover {
+  opacity: 0.8;
 }
 
 .theme-default {
@@ -183,6 +234,206 @@ body {
   background-color: #292c33;
 }
 .theme-default .theme-default {
+  border-color: #fff;
+}
+
+.theme-2 {
+  background-color: #233b23;
+  color: #bada55;
+}
+.theme-2 .mask {
+  background: #233b23;
+}
+.theme-2 .oom {
+  color: rgba(186, 218, 85, 0.4);
+}
+.theme-2 #light {
+  background: radial-gradient(
+    ellipse at center,
+    rgba(186, 218, 85, 0.5) 0%,
+    rgba(186, 218, 85, 0) 50%
+  );
+}
+.theme-2 .day {
+  border: 2px solid #233b23;
+}
+.theme-2 .day:after {
+  background-color: #233b23;
+  top: 3px;
+  bottom: 3px;
+  right: 3px;
+  left: 3px;
+}
+.theme-2 .day:not(.oom):hover {
+  background-color: #bada55;
+}
+.theme-2 .day:not(.oom):hover:after {
+  background-color: #1a2b1a;
+}
+.theme-2 .themes {
+  background-color: #233b23;
+}
+.theme-2 .theme-2 {
+  border-color: #fff;
+}
+
+.theme-3 {
+  background-color: #112;
+  color: #cd7bff;
+}
+.theme-3 .mask {
+  background: #112;
+}
+.theme-3 .oom {
+  color: rgba(205, 123, 255, 0.4);
+}
+.theme-3 #light {
+  background: radial-gradient(
+    ellipse at center,
+    rgba(205, 123, 255, 0.5) 0%,
+    rgba(205, 123, 255, 0) 50%
+  );
+}
+.theme-3 .day {
+  border: 2px solid #112;
+}
+.theme-3 .day:after {
+  background-color: #112;
+  top: 2px;
+  bottom: 2px;
+  right: 2px;
+  left: 2px;
+}
+.theme-3 .day:not(.oom):hover {
+  background-color: #cd7bff;
+}
+.theme-3 .day:not(.oom):hover:after {
+  background-color: #222244;
+}
+.theme-3 .themes {
+  background-color: #112;
+}
+.theme-3 .theme-3 {
+  border-color: #fff;
+}
+
+.theme-4 {
+  background-color: #cd7bff;
+  color: #111;
+}
+.theme-4 .mask {
+  background: #cd7bff;
+}
+.theme-4 .oom {
+  color: rgba(17, 17, 17, 0.4);
+}
+.theme-4 #light {
+  background: radial-gradient(
+    ellipse at center,
+    rgba(17, 17, 17, 0.5) 0%,
+    rgba(17, 17, 17, 0) 50%
+  );
+}
+.theme-4 .day {
+  border: 2px solid #cd7bff;
+}
+.theme-4 .day:after {
+  background-color: #cd7bff;
+  top: 1px;
+  bottom: 1px;
+  right: 1px;
+  left: 1px;
+}
+.theme-4 .day:not(.oom):hover {
+  background-color: #ba48ff;
+}
+.theme-4 .day:not(.oom):hover:after {
+  background-color: #c362ff;
+}
+.theme-4 .themes {
+  background-color: #cd7bff;
+}
+.theme-4 .theme-4 {
+  border-color: #fff;
+}
+
+.theme-5 {
+  background-color: #211;
+  color: #db2a2a;
+}
+.theme-5 .mask {
+  background: #211;
+}
+.theme-5 .oom {
+  color: rgba(219, 42, 42, 0.4);
+}
+.theme-5 #light {
+  background: radial-gradient(
+    ellipse at center,
+    rgba(219, 42, 42, 0.5) 0%,
+    rgba(219, 42, 42, 0) 50%
+  );
+}
+.theme-5 .day {
+  border: 2px solid #211;
+}
+.theme-5 .day:after {
+  background-color: #211;
+  top: 2px;
+  bottom: 2px;
+  right: 2px;
+  left: 2px;
+}
+.theme-5 .day:not(.oom):hover {
+  background-color: #db2a2a;
+}
+.theme-5 .day:not(.oom):hover:after {
+  background-color: #110909;
+}
+.theme-5 .themes {
+  background-color: #211;
+}
+.theme-5 .theme-5 {
+  border-color: #fff;
+}
+
+.theme-6 {
+  background-color: lightgoldenrodyellow;
+  color: rebeccapurple;
+}
+.theme-6 .mask {
+  background: lightgoldenrodyellow;
+}
+.theme-6 .oom {
+  color: rgba(102, 51, 153, 0.4);
+}
+.theme-6 #light {
+  background: radial-gradient(
+    ellipse at center,
+    rgba(0, 250, 154, 0.5) 0%,
+    rgba(0, 250, 154, 0) 50%
+  );
+}
+.theme-6 .day {
+  border: 2px solid lightgoldenrodyellow;
+}
+.theme-6 .day:after {
+  background-color: lightgoldenrodyellow;
+  top: 3px;
+  bottom: 3px;
+  right: 3px;
+  left: 3px;
+}
+.theme-6 .day:not(.oom):hover {
+  background-color: darksalmon;
+}
+.theme-6 .day:not(.oom):hover:after {
+  background-color: lightgoldenrodyellow;
+}
+.theme-6 .themes {
+  background-color: lightgoldenrodyellow;
+}
+.theme-6 .theme-6 {
   border-color: #fff;
 }
 
